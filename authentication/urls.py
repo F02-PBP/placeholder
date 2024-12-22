0492e6ec56b83c21df9ed7a53a65c230ea933592
gracekarinn <gracekarina12@gmail.com>
2024-12-22 14:48:37 +0700

    feat: add check_auth_status endpoint to authentication URLs

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from authentication.views import login, register, logout
+from authentication.views import login, register, logout, check_auth_status
 
 app_name = 'authentication'
 
 urlpatterns = [
     path('flutter-login/', login, name='login'),
     path('register/', register, name='register'),
-    path('logout/', logout, name='logout')
+    path('logout/', logout, name='logout'),
+    path('check_auth_status/', check_auth_status, name='check_auth_status'),
 ]

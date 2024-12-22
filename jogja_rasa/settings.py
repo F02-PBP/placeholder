f841052b0e9ccc8af3e3e2139808d2d1fd2474c0
gracekarinn <gracekarina12@gmail.com>
2024-12-22 13:42:01 +0700

    feat: update ALLOWED_HOSTS to allow all hosts and add logging configuration

--- jogja_rasa/settings.py
+++ jogja_rasa/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
     '10.0.2.2',
-    ''
+    '*',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
@@ -143,6 +143,20 @@ TIME_ZONE = 'Asia/Jakarta'
 USE_I18N = True
 
 USE_TZ = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+}
 
 
 # Static files (CSS, JavaScript, Images)
